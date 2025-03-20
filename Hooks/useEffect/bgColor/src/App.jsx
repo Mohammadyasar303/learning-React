25af7e92b4bc33e4fb78f57b86be6148fd509123
Yasar <yasarmansuri16@gmail.com>
2025-03-20 11:44:04 +0530

    somethings

---
+++ Hooks/useEffect/bgColor/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import BackgroundColor from './BackgroundColor'
+
+function App() {
+
+  return (
+    <>
+      <BackgroundColor/>
+    </>
+  )
+}
+
+export default App
---
+++ Hooks/useEffect/bgColor/src/BackgroundColor.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import "./App.css"
+
+function BackgroundColor() {
+  const [color, setbgColor] = useState('');
+
+  useEffect(() => {
+    document.body.style.backgroundColor = color;
+  }, [color]);
+
+  const buttonStyle = {className="d"};
+
+  return (
+    <div className='buttons'>
+      <button 
+        style={{...buttonStyle, color: '#ff0000'}}
+        onClick={() => setbgColor('red')}
+      >
+        Red
+      </button>
+      
+      <button 
+        style={{...buttonStyle, color: '#00ff00'}}
+        onClick={() => setbgColor('green')}
+      >
+        Green
+      </button>
+      
+      <button 
+        style={{...buttonStyle, color: '#800080'}}
+        onClick={() => setbgColor('purple')}
+      >
+        Purple
+      </button>
+    </div>
+  );
+}
+
+export default BackgroundColor;
